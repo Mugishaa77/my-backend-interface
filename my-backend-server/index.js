c366761bfa3a5259dc3cedfd24e17b6f89bf565f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-21 18:29:39 +0300

    saved nodemon content

--- my-backend-server/index.js
+++ my-backend-server/index.js
@@ -3,4 +3,15 @@ const cors = require ('cors');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("welcome to Evergreen")
+})
+
+const port = process.env.PORT || 5000
+
+console.log("Starting server...")
+
+app.listen(port, console.log(`server running on port ${port}`));
