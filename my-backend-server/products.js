ce5ddc2d3f480565074a330f780d8c040cea6bdc
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-28 23:04:27 +0300

    created a products array

--- my-backend-server/products.js
+++ my-backend-server/products.js
@@ -1,26 +1,27 @@
 const products = [
   {
-    id: 1,
+    id:    1,
     name: 'Tomatoes',
-    image: tomatoes,
+    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225880/Evergreen%20Farm%20Produce/produce/tomatoes_qf7hmx.webp",
     price: 10,
   },
   { 
-    id: 2,
+    id:    2,
     name: 'Garlic',
-    image: "",
+    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225908/Evergreen%20Farm%20Produce/produce/garlic_krnqfj.jpg",
     price: 10,
   },
   {
-    id:3,
+    id:    3,
     name: 'Red Onions',
-    image: "",
+    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225879/Evergreen%20Farm%20Produce/produce/red-onions_lbzxz0.jpg",
     price: 10,
   },
   {
+    id:   4,
     name: 'Sagaa',
-    image: "",
-    price: 'Ksh 10.00',
+    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225882/Evergreen%20Farm%20Produce/produce/sagaa_q95lhr.jpg",
+    price: 10,
   },
 ];
 
