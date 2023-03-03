bea180c43b375045709c679a1561761179044ebf
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-03 19:51:00 +0300

    added a login system

--- my-backend-server/index.js
+++ my-backend-server/index.js
@@ -1,23 +1,62 @@
 const express = require('express');
-const cors = require ('cors');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
-const products = require("./products");
+const users = [
+  {
+    id: 1,
+    username: 'customer',
+    password: 'customer123',
+    role: 'customer',
+  },
+  {
+    id: 2,
+    username: 'farmer',
+    password: 'farmer123',
+    role: 'farmer',
+  },
+  {
+    id: 3,
+    username: 'grocer',
+    password: 'grocer123',
+    role: 'grocer',
+  },
+];
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("welcome to Evergreen");
+app.get('/', (req, res) => {
+  res.send('Welcome to Evergreen');
 });
 
-app.get("/products", (req, res) => {
-    res.send(products);
+app.get('/products', (req, res) => {
+  res.send(products);
 });
 
-const port = process.env.PORT || 5000
+app.post('/login', (req, res) => {
+  // Get username and password from request body
+  const { username, password } = req.body;
 
+  // Find user in the users array
+  const user = users.find(u => u.username === username && u.password === password);
 
+  if (!user) {
+    // Return error response if user not found
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
 
-app.listen(port, console.log(`server running on port ${port}`));
+  // Generate JWT token with user id and role as payload
+  const token = jwt.sign({ id: user.id, role: user.role }, 'secret');
+
+  // Return token as response
+  res.json({ token });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
