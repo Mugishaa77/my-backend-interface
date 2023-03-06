8fc2ab2c40e7e8757a417c6808e5b857b2bf9e13
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-06 13:31:57 +0300

    completed user authentication

--- my-backend-server/index.js
+++ my-backend-server/index.js
@@ -6,23 +6,15 @@ const app = express();
 const users = require('./login');
 const products = require('./products');
 
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const uri = "mongodb://localhost:27017/my_database";
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-  if(err) {
-    console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-  }
-  console.log('Connected...');
-
-  const collection = client.db("my_database").collection("user data");
-
-  // perform a query
-  collection.find({}).toArray(function(err, docs) {
-    console.log(docs);
-    client.close();
-  });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB database');
 });
 
 const userSchema = new mongoose.Schema({
@@ -34,41 +26,16 @@ const userSchema = new mongoose.Schema({
   county: String,
   email: {
     type: String,
-    unique: true, // Ensure emails are unique
-    required: true, // Ensure emails are required
+    unique: true,
+    required: true,
   },
   password: {
     type: String,
-    required: true, // Ensure passwords are required
+    required: true,
   },
 });
 
-
 const User = mongoose.model('User', userSchema);
-
-app.post('/signup', async (req, res) => {
-  const { firstName, lastName, idNumber, city, telephone, county, email, password } = req.body;
-
-  try {
-    // Check if user with email already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: 'User with email already exists' });
-    }
-
-    // Create new user object
-    const user = new User({ firstName, lastName, idNumber, city, telephone, county, email, password });
-
-    // Save user object to database
-    await user.save();
-
-    // Return success message
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
 
 app.use(express.json());
 app.use(cors());
@@ -81,26 +48,48 @@ app.get('/products', (req, res) => {
   res.send(products);
 });
 
-app.get('/login', (req, res) => {
-  res.send(users);
+app.post('/signup', async (req, res) => {
+  const { firstName, lastName, idNumber, city, telephone, county, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User with email already exists' });
+    }
+
+    const user = new User({ firstName, lastName, idNumber, city, telephone, county, email, password });
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
 
-  
-  const user = users.find(u => u.username === username && u.password === password);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
 
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid username or password' });
+    const isPasswordValid = password === user.password;
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ id: user._id }, 'secret');
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
-
-  
-  const token = jwt.sign({ id: user.id, role: user.role }, 'secret');
-
-  
-  res.json({ token });
 });
 
 const port = process.env.PORT || 5000;
