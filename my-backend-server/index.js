100202c94064bc8a2272f0cc2f81999d1c3d6654
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-08 11:17:29 +0300

    Added an env file

--- my-backend-server/index.js
+++ my-backend-server/index.js
@@ -4,11 +4,8 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-
 const products = require('./products');
 require("dotenv").config()
-
-
 
 app.use(express.json());
 app.use(cors());
@@ -21,12 +18,10 @@ app.get('/products', (req, res) => {
   res.send(products);
 });
 
-
-
-
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+mongoose.connect()
