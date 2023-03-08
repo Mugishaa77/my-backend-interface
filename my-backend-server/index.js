01d1a28376fa20e5b6ba5608402070fc291644c8
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-08 10:27:19 +0300

    configured dot.env

--- my-backend-server/index.js
+++ my-backend-server/index.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-const users = require('./login');
+
 const products = require('./products');
+require("dotenv").config()
 
-const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/my_database";
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB database');
-});
-
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  idNumber: String,
-  city: String,
-  telephone: String,
-  county: String,
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-const User = mongoose.model('User', userSchema);
 
 app.use(express.json());
 app.use(cors());
@@ -50,49 +23,7 @@ app.get('/products', (req, res) => {
 
 
 
-app.post('/sign-up', async (req, res) => {
-  const { firstName, lastName, idNumber, city, telephone, county, email, password } = req.body;
 
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: 'User with email already exists' });
-    }
-
-    const user = new User({ firstName, lastName, idNumber, city, telephone, county, email, password });
-    await user.save();
-
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const isPasswordValid = password === user.password;
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const token = jwt.sign({ id: user._id }, 'secret');
-
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
 
 const port = process.env.PORT || 5000;
 
