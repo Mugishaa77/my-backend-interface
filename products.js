a1c4733b0df59dd1861bed40e148d2df7b0d07a3
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-03 13:47:22 +0300

    fixed a bug

--- products.js
+++ products.js
@@ -2,25 +2,25 @@ const products = [
   {
     id:    1,
     name: 'Tomatoes',
-    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225880/Evergreen%20Farm%20Produce/produce/tomatoes_qf7hmx.webp",
+    image: "https://res.cloudinary.com/sallymugisha/image/upload/v1677225880/Evergreen%20Farm%20Produce/produce/tomatoes_qf7hmx.webp",
     price: 10,
   },
   { 
     id:    2,
     name: 'Garlic',
-    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225908/Evergreen%20Farm%20Produce/produce/garlic_krnqfj.jpg",
+    image: "https://res.cloudinary.com/sallymugisha/image/upload/v1677225908/Evergreen%20Farm%20Produce/produce/garlic_krnqfj.jpg",
     price: 10,
   },
   {
     id:    3,
     name: 'Red Onions',
-    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225879/Evergreen%20Farm%20Produce/produce/red-onions_lbzxz0.jpg",
+    image: "https://res.cloudinary.com/sallymugisha/image/upload/v1677225879/Evergreen%20Farm%20Produce/produce/red-onions_lbzxz0.jpg",
     price: 10,
   },
   {
     id:   4,
     name: 'Sagaa',
-    image: "https://res.cloudinary.com/dbg2azzwt/image/upload/v1677225882/Evergreen%20Farm%20Produce/produce/sagaa_q95lhr.jpg",
+    image: "https://res.cloudinary.com/sallymugisha/image/upload/v1677225882/Evergreen%20Farm%20Produce/produce/sagaa_q95lhr.jpg",
     price: 10,
   },
 ];
