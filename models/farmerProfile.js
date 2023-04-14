318bce72a03b1f664a95c01081bb29bfd891c1eb
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-14 14:16:23 +0300

    added a schema for the farmer and grocer profile

---
+++ models/farmerProfile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const farmerProfileSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  emailAddress: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+  stallName: { type: String, required: true },
+  farmerStallNumber: { type: String, required: true },
+});
+
+const FarmerProfile = mongoose.model('FarmerProfile', farmerProfileSchema);
+
+module.exports = FarmerProfile;
---
+++ models/grocerProfile.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const grocerProfileSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  emailAddress: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+  stallName: { type: String, required: true },
+  grocerStallNumber: { type: String, required: true },
+});
+
+const GrocerProfile = mongoose.model('GrocerProfile', grocerProfileSchema);
+
+module.exports = GrocerProfile;
