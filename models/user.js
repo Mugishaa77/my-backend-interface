d8719f7aaeed291d52b074ea083dc0687f4c8994
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-26 18:29:23 +0300

    rewrote the registration process

--- models/user.js
+++ models/user.js
@@ -3,15 +3,12 @@ const mongoose = require ('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true, minLength: 3, maxLength: 30},
     lastName: {type: String, required: true, minLength: 3, maxLength: 30},
+    idNumber: {type: String, required: true, minLength: 3, maxLength: 30},
+    phoneNumber: {type: String, required: true, minLength: 3, maxLength: 14},
     email: {type: String, required: true, minLength: 3, maxLength: 200, unique:true},
     password: {type: String, required: true, minLength: 3, maxLength: 1024},
-    idNumber: {type: String, required: true, minLength: 3, maxLength: 30},
-    telephone: {type: String, required: true, minLength: 10, maxLength: 14},
-     city: {type: String, required: true, minLength: 3, maxLength: 30},
-    county: {type: String, required: true, minLength: 3, maxLength: 30},
+    role: {type: String, required:true, }
     
-
-
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/register.js
+++ routes/register.js
@@ -1,54 +1,17 @@
-const bcrypt = require('bcryptjs');
-const Joi = require('joi');
 const express = require('express');
-const genAuthToken = require('../utils/genAuthToken');
-
 const router = express.Router();
+const { User } = require('../models/user');
 
-router.post("/", async(req, res) => {
-    
-    const schema = Joi.object({
-        firstName: Joi.string().min(3).max(30).required(),
-        lastName: Joi.string().min(3).max(30).required(),
-        idNumber: Joi.string().min(3).max(30).required(),
-        city: Joi.string().min(3).max(30).required(),
-        telephone: Joi.string().min(10).max(14).required(),
-        county: Joi.string().min(3).max(30).required(),
-        email: Joi.string().min(3).max(200).required().email(),
-        password: Joi.string().min(3).max(200).required()
-
-    });
-    
-    const {error} = schema.validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    let user = user.findOne({email: req.body.email});
-    if(user) return res.status(400).send("User already exists");
-
-    user = new User ({
-        firstName : req.body.firstName,
-        lastName: req.body.lastName,
-        idNumber: req.body.idNumber,
-        telephone: req.body.telephone,
-        county: req.body.county,
-        city: req.body.city,
-        email: req.body.email,
-        password: req.body.password,
-        
-        
-    
-    });
-
-    const salt = await bcrypt.genSalt(10);
-
-    user.password = await bcrypt.hash(user.password, salt)
-
-
-    user = await user.save()
-
-    const token = genAuthToken(user);
-
-    res.send(token);
+router.post('/register', async (req, res) => {
+  try {
+    const { firstName, lastName, idNumber, phoneNumber, email, password, role } = req.body;
+    const user = new User({ firstName, lastName, idNumber, phoneNumber, email, password, role });
+    await user.save();
+    res.send('User registered successfully!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error registering user!');
+  }
 });
 
 module.exports = router;
