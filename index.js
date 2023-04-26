bb55be7a1ff714098094cccab13f7df1996fefe5
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-26 20:28:52 +0300

    redefined routes for registration

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // api end-points(used /api prefixing for code best practices)
+
 app.use('/users', users);
 app.use("/api/login", login);
 app.use("/api/products", productsRoute);
